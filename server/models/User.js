d065df0b939e938d5a74f12ae31cb009b2241e6e
= <=>
2021-06-02 20:32:31 +0300

    add detail to user

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,10 @@ const schema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   firstName: String,
   lastName: String,
+  city: String,
+  country: String,
+  about: String,
+  birthday:Date
 });
 
 module.exports = mongoose.model('User', schema);
