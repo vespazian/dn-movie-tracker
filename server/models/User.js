872fbd07871680ea40fbec1b25ce031086439e5c
= <=>
2021-06-05 15:49:45 +0300

    add route for patch and put

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true }
-  /*,
+  email: { type: String, unique: true, required: true },
   firstName: { type: String},
   lastName: { type: String},
   city: { type: String},
   country: { type: String},
   about: { type: String},
-  birthday:{ type: Date}*/
+  birthday:{ type: Date}
 });
 
 module.exports = mongoose.model('User', schema);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,73 +1,106 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 
 const router = express.Router();
 
 //întoarce toți utilizatorii
-router.get('/', async (req, res) => {
-  try{
-  const user = await User.find();
-  // find will return all user's object or null
-  
-    res.json(user);
-  } catch(error) {
-  
-    res.status(404).send('User not found');
-  }
- 
-});
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.find();
 
-//întoarce un singur utilizator
-router.get('/:email', async (req, res) => {
-  try{
-  const user = await User.findOne({ email: req.params.email });
-  // findOne will return the found user object or null
-  
     res.json(user);
-  } catch(error) {
-  
-    res.status(404).send('User not found');
+  } catch (error) {
+    res.status(404).send("User not found");
   }
- 
 });
 
 //adaugă un utilizator
-router.post('/', async (req, res) => {
-  // create will try to create a new user, but can throw an error if there is already an user with the same email (unique param in the schema)
+router.post("/", async (req, res) => {
   try {
-    const user = await User.create({ email: req.body.email} );
+    const user = await User.create({
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      city: req.body.city,
+      about: req.body.about,
+      birthday: req.body.birthday
+    });
     res.send(user);
   } catch (error) {
     res.status(400).send(error + "pe linia de post-create");
   }
 });
 
-//delete by _id
-router.delete('/:_id', async (req, res) => {
-  try{
-  const user = await User.findByIdAndRemove({ _id: req.params._id });
-  // findByIdAndRemove find and remove
-  
-    res.send(user);
-  } catch(error) {
-  
-    res.status(404).send( error + 'User not found to delete');
+//întoarce un singur utilizator
+router.get("/:email", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    // findOne will return the found user object or null
+
+    res.json(user);
+  } catch (error) {
+    res.status(404).send("User not found");
   }
- 
+});
+
+//modifică datele unui utilizator
+router.put("/:email", async (req, res) => {
+  try {
+    const user = await User.findOneAndReplace({email: req.params.email}, req.body
+    );
+    res.json( user );
+  } catch (error) {
+    res.status(400).send(error + "pe ruta put - findOneAndReplace");
+  }
+});
+
+//suprascrie un utilizator
+router.patch("/:email", async (req, res) => {
+  try {
+    const modif = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      city: req.body.city,
+      about: req.body.about,
+      birthday: req.body.birthday
+    }
+
+    const user = await User.findOneAndUpdate({email: req.params.email}, modif
+    );
+    res.json( user );
+  } catch (error) {
+    res.status(400).send(error + "pe ruta patch -findOneAndUpdate ");
+  }
 });
 
 //șterge un utilizator // delete by email
-router.delete('/email/:email', async (req, res) => {
-  try{
-  const user = await User.findOneAndRemove({ email: req.params.email });
-  // findByIdAndRemove find and remove
-  
-    res.send(user);
-  } catch(error) {
-  
-    res.status(404).send( error + 'User not found to delete');
+router.delete("/:email", async (req, res) => {
+  try {
+    const user = await User.findOneAndRemove({ email: req.params.email });
+    // findByIdAndRemove find and remove
+    if(user) {
+    res.send(user);}
+    else {
+      res.send("userul nu a fost gasit")
+    }
+  } catch (error) {
+    res.status(404).send(error + "User not found to delete");
   }
- 
 });
 
+
+/* delete by _id
+router.delete("/:_id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndRemove({ _id: req.params._id });
+    // findByIdAndRemove find and remove
+
+    res.send(user);
+  } catch (error) {
+    res.status(404).send(error + "User not found to delete");
+  }
+});
+
+with help from https://www.djamware.com/post/59faec0a80aca7739224ee1f/building-crud-web-application-using-mern-stack#routes-rest-api
+*/
 module.exports = router;
